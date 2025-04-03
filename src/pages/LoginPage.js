663b1923ccef56dd46c4759fad1c5a888c265e2c
SEUNGHWA LEE <sw092311@cau.ac.kr>
2025-04-03 14:59:33 +0900

    feat: 로그인 페이지 및 로고 사진 추가

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "../components/Button";
+import Logo from "../components/Logo";
+
+const LoginPage = () => {
+    const handleKakaoLogin = () => {
+        //window.location.href = "나중에 카카오 로그인 키. "
+    };
+
+    return (
+        <div style = {styles.container}>
+            <Logo />
+            <h2 style={styles.title} >회원가입하기</h2>
+            <button style={styles.signupButton}>SIGN UP</button>
+            <Button text="카카오 로그인" color="#FEE500" onClick={handleKakaoLogin} icon="/images/kakao_login.svg"/>
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        display : "flex", 
+        flexDirection : "column",
+        alignItems : "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#FAE8D4",
+    },
+    title: {
+        fontSize : "20px",
+        marginBottom: "20px",
+    },
+    signupButton: {
+        backgroundColor: "transparent",
+        border: "1px solid black",
+        padding: "10px 20px",
+        borderRadius: "8px",
+        fontSize: "16px",
+        marginBottom: "20px",
+        cursor: "pointer",
+    },
+};
+
+export default LoginPage;
