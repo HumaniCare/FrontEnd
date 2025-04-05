42fc622cdc1ded7311d65d2b3dba1a6ace52af41
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-04-05 17:18:48 +0900

    axios api, voice reocrder 설정 (#7)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import VoiceTrainingPage from "./pages/VoiceTrainingPage";
+import KeywordSelectionPage from "./pages/KeywordSelectionPage";
+import FinalPage from "./pages/FinalPage";
+
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignupPage/>} />
+        <Route path="/voice-training" element={<VoiceTrainingPage/>} />
+        <Route path="/keywords" element={<KeywordSelectionPage/>} />
+        <Route path="/final" element={<FinalPage/>} />
       </Routes>
     </Router>
   );
---
+++ src/constants/api.js
@@ -0,0 +1,2 @@
+export const LOCAL_SPRING_API_URL = "http://localhost:8080/api/spring";
+export const LOCAL_FASTAPI_API_URL = "http://localhost:8000/api/fastapi";
