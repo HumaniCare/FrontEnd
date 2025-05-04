a4386688872f03b57204bfce9df29b7b082ef817
SEUNGHWA LEE <sw092311@cau.ac.kr>
2025-05-04 12:19:43 +0900

    LoginPage 카카오 로그인 이미지 수정

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,45 +1,49 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "../components/Button";
-import Logo from "../components/Logo";
 import { SPRING_API_URL } from "../constants/api";
+import Logo from "../components/Logo";
 
 const LoginPage = () => {
-    const navigate = useNavigate();
     const handleKakaoLogin = () => {
-        window.location.href = `${ SPRING_API_URL}/oauth/kakao`;
+        window.location.href = `${SPRING_API_URL}/oauth/kakao`;
     };
 
     return (
-        <div style = {styles.container}>
+        <div style={styles.container}>
             <Logo />
-            <h2 style={styles.title} >회원가입하기</h2>
-            <button style={styles.signupButton}>SIGN UP</button>
-            <Button text="카카오 로그인" color="#FEE500" onClick={handleKakaoLogin} icon="/images/kakao_login.svg"/>
+            <p style={styles.message}>
+                혼자 계신 부모님이 걱정되시나요? <br />
+                휴머니케어로 함께하세요.
+            </p>
+            <img
+                src={process.env.PUBLIC_URL + "/images/kakao_login.svg"}
+                alt="카카오 로그인 버튼"
+                style={styles.kakaoButton}
+                onClick={handleKakaoLogin}
+            />
         </div>
     );
 };
 
 const styles = {
     container: {
-        display : "flex", 
-        flexDirection : "column",
-        alignItems : "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         justifyContent: "center",
         height: "100vh",
         backgroundColor: "#FAE8D4",
+        padding: "0 20px",
+        textAlign: "center",
     },
-    title: {
-        fontSize : "20px",
-        marginBottom: "20px",
+    message: {
+        fontSize: "18px",
+        color: "#333",
+        margin: "30px 0 40px",
+        lineHeight: "1.5",
+        fontWeight: "500",
     },
-    signupButton: {
-        backgroundColor: "transparent",
-        border: "1px solid black",
-        padding: "10px 20px",
-        borderRadius: "8px",
-        fontSize: "16px",
-        marginBottom: "20px",
+    kakaoButton: {
+        width: "250px",
         cursor: "pointer",
     },
 };
