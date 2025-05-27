5c74327e9afb404b342223e124ffdb9319da9bd8
sjpark1023 <babyy1023@naver.com>
2025-05-27 13:01:20 +0900

    fix: API 경로 올바르게 수정

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const LOCAL_SPRING_API_URL = "http://localhost:8080/api/spring";
 export const LOCAL_FASTAPI_API_URL = "http://localhost:8000/api/fastapi";
 
-export const SPRING_API_URL = "https://www.humanicare.store/api/spring";
-export const FASTAPI_API_URL = "https://www.humanicare.store/api/fastapi";
+export const SPRING_API_URL = "https://springboot:8080/api/spring";
+export const FASTAPI_API_URL = "https://fastapi:8000/api/fastapi";
